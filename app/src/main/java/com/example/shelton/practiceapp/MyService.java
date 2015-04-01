2d6f7aa2d65ec7defbbace548fa46f1ddb11bb52
EnzanIjuin <protomanexe23@gmail.com>
2015-04-01 00:33:38 -0700

    Reminder stuff

---
+++ app/src/main/java/com/example/shelton/practiceapp/MyService.java
@@ -0,0 +1,18 @@
+package com.example.shelton.practiceapp;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class MyService extends Service {
+
+    public MyService() {
+    }
+
+
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
