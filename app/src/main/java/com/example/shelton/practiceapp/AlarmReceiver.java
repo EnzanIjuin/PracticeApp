3e89f2f536d9e848677c98a48177d254aedd2ddc
EnzanIjuin <protomanexe23@gmail.com>
2015-04-03 19:58:30 -0700

    Reminder complete

--- app/src/main/java/com/example/shelton/practiceapp/AlarmReceiver.java
+++ app/src/main/java/com/example/shelton/practiceapp/AlarmReceiver.java
@@ -1,8 +1,10 @@
 package com.example.shelton.practiceapp;
 
+import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
 
 public class AlarmReceiver extends BroadcastReceiver {
     public AlarmReceiver() {
@@ -11,5 +13,17 @@ public class AlarmReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
 
+        //PendingIntent notifyIntent = PendingIntent.getActivity(context, 0, new Intent(context, MainActivity.class), 0);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
+                .setSmallIcon(R.drawable.cutemon)
+                .setContentTitle("Reminder")
+                .setContentText(intent.getStringExtra("desc"));
+
+        builder.setDefaults(NotificationCompat.DEFAULT_SOUND);
+        builder.setAutoCancel(true);
+
+        NotificationManager manager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        manager.notify(1, builder.build());
     }
 }
--- app/src/main/java/com/example/shelton/practiceapp/CalendarFragment.java
+++ app/src/main/java/com/example/shelton/practiceapp/CalendarFragment.java
@@ -1,11 +1,14 @@
 package com.example.shelton.practiceapp;
 
+import android.app.AlarmManager;
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,7 +16,8 @@ import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TimePicker;
-import android.widget.Toast;
+
+import java.util.GregorianCalendar;
 
 
 public class CalendarFragment extends Fragment implements View.OnClickListener, DialogInterface.OnClickListener{
@@ -46,13 +50,7 @@ public class CalendarFragment extends Fragment implements View.OnClickListener, 
 
         builder.setTitle(R.string.title_dialog_reminder);
         builder.setView(inflater.inflate(R.layout.dialog_set_reminder, null));
-        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                dialogInterface.cancel();
-            }
-        });
-
+        builder.setNegativeButton("Cancel", this);
         builder.setPositiveButton("OK", this);
 
         builder.show();
@@ -60,12 +58,33 @@ public class CalendarFragment extends Fragment implements View.OnClickListener, 
 
     @Override
     public void onClick(DialogInterface dialogInterface, int i) {
-        Dialog dialog = (Dialog) dialogInterface;
+        if(i == DialogInterface.BUTTON_POSITIVE) {
+            // To allow findById in dialog.
+            Dialog dialog = (Dialog) dialogInterface;
 
-        EditText desc = (EditText) dialog.findViewById(R.id.in_reminder);
-        TimePicker timePicker = (TimePicker) dialog.findViewById(R.id.timePicker_reminder);
-        DatePicker datePicker = (DatePicker) getActivity().findViewById(R.id.datePicker_reminder);
+            EditText desc = (EditText) dialog.findViewById(R.id.in_reminder);
+            TimePicker timePicker = (TimePicker) dialog.findViewById(R.id.timePicker_reminder);
+            DatePicker datePicker = (DatePicker) getActivity().findViewById(R.id.datePicker_reminder);
 
+            // Alarm time format.
+            GregorianCalendar alarmTime = new GregorianCalendar(datePicker.getYear(),
+                    datePicker.getMonth(), datePicker.getDayOfMonth(), timePicker.getCurrentHour(),
+                    timePicker.getCurrentMinute());
+
+            Intent alarmIntent = new Intent(getActivity(), AlarmReceiver.class);
+            alarmIntent.putExtra("desc", desc.getText().toString());
+
+            AlarmManager alarmManager = (AlarmManager) getActivity()
+                    .getSystemService(Context.ALARM_SERVICE);
+
+            alarmManager.set(AlarmManager.RTC_WAKEUP, alarmTime.getTimeInMillis(),
+                    PendingIntent.getBroadcast(getActivity(), 1, alarmIntent,
+                            PendingIntent.FLAG_ONE_SHOT));
+
+            dialogInterface.dismiss();
+        } else {
+            dialogInterface.cancel();
+        }
 
     }
 }
